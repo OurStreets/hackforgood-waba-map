824d8cb5bbd8b187d31fb81a33d3f039a6e49a42
Chris Slatt <chris@dodgersden.com>
2020-03-06 19:45:48 -0500

    Add files via upload
    Fixed concurrency bug where both filtered and mapped facilities were getting pushed into the same array.

--- scripts/combine.js
+++ scripts/combine.js
@@ -3,24 +3,25 @@ var path = require('path');
 var request = require('request');
 var merger = require('@mapbox/geojson-merge');
 
-var myfiles = [];
+var mappedfiles = [];
+var filteredfiles = [];
 
 fs.readdir('mappedfacilities', function (err, files) { if (err) throw err;
   files.forEach( function (file) {
-    myfiles.push(path.resolve('mappedfacilities', file));
+    mappedfiles.push(path.resolve('mappedfacilities', file));
     console.log(file);
   });
-  var mergedStream = merger.mergeFeatureCollectionStream(myfiles);
+  var mergedStream = merger.mergeFeatureCollectionStream(mappedfiles);
   var mergedFile = fs.createWriteStream(path.resolve('mappedfacilities', 'all.geojson'));
   mergedStream.pipe(mergedFile);
 });
 
 fs.readdir('filteredfacilities', function (err, files) { if (err) throw err;
   files.forEach( function (file) {
-    myfiles.push(path.resolve('filteredfacilities', file));
+    filteredfiles.push(path.resolve('filteredfacilities', file));
     console.log(file);
   });
-  var mergedStream = merger.mergeFeatureCollectionStream(myfiles);
+  var mergedStream = merger.mergeFeatureCollectionStream(filteredfiles);
   var mergedFile = fs.createWriteStream(path.resolve('filteredfacilities', 'all.geojson'));
   mergedStream.pipe(mergedFile);
 });
